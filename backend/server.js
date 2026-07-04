58bb9acf58a1fd818113e4a4fdf1742f9e0bd658
Rishitha Kaluvala <your-email@gmail.com>
2026-07-04 21:45:21 +0530

    Added response compression

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const compression = require("compression");
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 
 /* ============================
